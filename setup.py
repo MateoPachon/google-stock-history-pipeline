ef8489c19f99e73b1650751fc68ee01f03970d3d
Mateo Pachón <159659978+MateoPachon@users.noreply.github.com>
2025-05-06 03:07:02 +0000

    carga inicial

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="google-stock-history-pipeline",
+    version="0.0.1",
+    author="Mateo Pachón",
+    author_email="",
+    description="",
+    py_modules=["actividad_1","actividad_2"],
+    install_requires=[
+        "pandas",
+        "numpy",
+        "openpyxl",
+        "requests"
+    ]
+) 
